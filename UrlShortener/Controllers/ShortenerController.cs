3d6c5408e780c6e528ab122acb96a2f2354e395b
Artem Vasilyev <artem001v@gmail.com>
2022-02-18 21:45:16 +0300

    Add project files.

---
+++ UrlShortener/Controllers/ShortenerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UrlShortener.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ShortenerController : ControllerBase
+    {
+        
+    }
+}
